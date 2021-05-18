ec2c6131faee947d3b4824a1b4e4ace565876e0b
Penguin135 <rudwns273@naver.com>
2021-05-18 03:23:09 +0000

    first example

---
+++ terraform/ncp-example-one-vm/main.tf
@@ -0,0 +1,17 @@
+provider "ncloud" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
+}
+
+data "ncloud_regions" "regions" {
+}
+
+data "ncloud_server_images" "server_images" {
+}
+
+resource "ncloud_server" "server" {
+  name                      = var.server_name
+  server_image_product_code = var.server_image_product_code
+  server_product_code       = var.server_product_code
+}
