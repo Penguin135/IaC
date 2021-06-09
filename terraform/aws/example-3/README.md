93259d56ea6312cf4434a9becba4b2ed095f3f0d
Penguin135 <rudwns273@naver.com>
2021-06-09 07:28:02 +0000

    update main.tf README.md

--- terraform/aws/example-3/README.md
+++ terraform/aws/example-3/README.md
@@ -1,1 +1,23 @@
-# test is processing
+# 생성되는 리소스
+- Key Pair
+- Lightsail Instance (ubuntu 18.04, 1 cpu, 1 GB memory)
+- Lightsail public ports (22, 80, 3306)
+
+# 실습 진행에 참고한 사이트
+- https://www.44bits.io/ko/keyword/amazon-lightsail
+- https://docs.aws.amazon.com/ko_kr/cli/latest/userguide/install-cliv2-linux.html
+- https://lightsail.aws.amazon.com/ls/docs/en_us/articles/amazon-lightsail-managing-access-for-an-iam-user
+- https://docs.aws.amazon.com/cli/latest/reference/lightsail/get-blueprints.html
+- https://aws.amazon.com/ko/premiumsupport/knowledge-center/lightsail-aws-cli-commands/
+- https://not-to-be-reset.tistory.com/368
+- https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lightsail_key_pair
+- https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lightsail_instance
+- https://a1010100z.tistory.com/174
+- https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lightsail_instance_public_ports
+- https://lightsail.aws.amazon.com/ls/docs/ko_kr/articles/understanding-firewall-and-port-mappings-in-amazon-lightsail
+
+# 실습에 사용한 프로그램
+- aws cli version 2
+
+# 실습 정리 URL
+- https://not-to-be-reset.tistory.com/440
--- terraform/aws/example-3/main.tf
+++ terraform/aws/example-3/main.tf
@@ -1,6 +1,6 @@
-resource "aws_lightsail_key_pair" "lightsail-test" { # lightsail-test라는 이름의 aws_lightsail_key_pair 라는 타입의 리소스를 정의한다.
+resource "aws_lightsail_key_pair" "lightsail-test" { # lightsail-test라는 이름의 aws_lightsail_key_pair 라는 타입의 리소스를 정의
   name = "lightsail-test" # 생성될 lightsail 키페어의 이름
-  public_key = file("~/.ssh/lightsail_key.pub") # 키페어에 사용할 public key 지정
+  public_key = file("./lightsail_key.pub") # 키페어에 사용할 public key 지정
 }
 
 # Create a new Lightsail Instance
@@ -13,4 +13,25 @@ resource "aws_lightsail_instance" "lightsail_test" {
   tags = {
     Name = "my-lightsail"
   }
+}
+
+resource "aws_lightsail_instance_public_ports" "lightsail_firewall_test" { # lightsail_firewall_test 라는 이름의 aws_lightsail_instance_public_ports 라는 타입의 리소스 정의
+    instance_name = aws_lightsail_instance.lightsail_test.name # 적용할 lightsail instance의 이름
+
+    # port information
+    port_info {
+            protocol = "tcp"
+            from_port = 80
+            to_port = 0
+    } 
+    port_info {
+            protocol = "tcp"
+            from_port = 22
+            to_port = 0
+    }
+    port_info {
+            protocol = "tcp"
+            from_port = 3306
+            to_port = 0
+    }
 }
